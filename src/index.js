50de1c8e0819ad88f97162b832a3e7cd9713bcb8
Alexandr Samusevich <36624664+19alexandr90@users.noreply.github.com>
2018-03-01 17:47:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = bracketsConfig.map(pair => pair.join(''));
+    let foundSmth = true;
+    while (str.length && foundSmth) {
+        foundSmth = false;        
+        for (let i = 0; i < brackets.length; i++) {
+            let foundedPos = str.indexOf(brackets[i]);
+            if (foundedPos != -1) {
+                str = str.slice(0, foundedPos) + str.slice(foundedPos + 2);
+                foundSmth = true;
+            }
+        }
+    }
+    return str.length ? false : true;
 }
